d8da1d0b914ecd8c3442448ab860c851c40d1597
Richie <ofotsurich@gmail.com>
2025-10-02 15:32:30 +0000

    create an investment calculator

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Header from "./componets/Header"
+import UserInput from "./componets/UserInput"
+import Results from "./componets/Results";
+
+function App() {
+    const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  
+  const inputIsValid = userInput.duration >0
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+  return (<>
+    <Header />
+    <UserInput userInput={userInput} handleChange={handleChange}/>
+    {!inputIsValid && <p className="center ">Please Duration can not be negative </p>}
+    {inputIsValid && <Results input={userInput}/>}
+    </>
+  )
+}
+
+export default App
